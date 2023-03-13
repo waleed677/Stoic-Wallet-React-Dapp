ca71dd41354b0fdbe48b8ac929786cec42a53889
Waleed Arshad Awan <waleedmalik677@gmail.com>
2023-03-14 01:52:10 +0500

    add stoic plugin sdk

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,51 +7,60 @@ import { StoicIdentity } from "ic-stoic-identity";
 import Loader from '../component/Loader';
 import { Actor, HttpAgent } from '@dfinity/agent';
 import { idlFactory } from './did.js';
-
+import Alert from 'react-bootstrap/Alert';
 function Home() {
 
     const [loader, setLoader] = useState(true);
     const [connected, setConnected] = useState(false);
     const [address, setAddress] = useState("");
+    const [holdNFT, setHoldNFT] = useState(-1);
 
     const canisterId = 'x7z74-uiaaa-aaaag-qbsnq-cai'; // the ID of your canister
-
+    const host = 'https://ic0.app';
     async function connectWallet() {
 
         try {
-          const identity = await StoicIdentity.load();
-          if (identity !== false) {
-            // No existing connection, let's make one
-           
-            setAddress(identity.getPrincipal().toText());
-            setConnected(true);
-          } else {
-            // ID is already connected
-            const newIdentity = await StoicIdentity.connect();
-            setAddress(newIdentity.getPrincipal().toText());
-            setConnected(true);
-          }
+            const identity = await StoicIdentity.load();
+            if (identity !== false) {
+                setAddress(identity.getPrincipal().toText());
+                setConnected(true);
+            } else {
+                const newIdentity = await StoicIdentity.connect();
+                setAddress(newIdentity.getPrincipal().toText());
+                setConnected(true);
+            }
 
-         const actor = Actor.createActor(idlFactory, {
-            agent: new HttpAgent({
-                identity,
-              }),
-              canisterId,
-         });
+            setLoader(true);
+            const actor = Actor.createActor(idlFactory, {
+                agent: new HttpAgent({ identity, host: host }),
+                canisterId,
+            });
+
+            const nfts = await actor.listings();
 
 
-         console.log({actor});
+            // loop through all nfts and get each nft principle
 
-         const nfts = await actor.listings();
-         console.log({ nfts });
-         
+            nfts.forEach((nft) => {
+                const sellerBytes = nft[1].seller;
+                let seller = sellerBytes.toString();
+
+                if (seller != address) {
+                    setHoldNFT(false);
+                    setLoader(false);
+                } else {
+                    setHoldNFT(true);
+                    setLoader(false);
+                }
+            });
 
 
         } catch (error) {
-          console.log(`Error connecting to wallet: ${error.message}`);
-          setConnected(false);
+            console.log(`Error connecting to wallet: ${error.message}`);
+            setConnected(false);
+            setLoader(false);
         }
-      }
+    }
 
     const disconnectWallet = () => {
         StoicIdentity.disconnect();
@@ -67,7 +76,6 @@ function Home() {
 
     }, [])
 
-
     return (
         <>
 
@@ -79,11 +87,15 @@ function Home() {
                 </div>
 
                 <div className="text-center mt-5">
-                {connected &&   <h5 className='font-italic'>Your Princple ID is : {address}</h5> }
+                    {connected && <h5 className='font-italic'>Your Principle ID is : {address}</h5>}
+                </div>
+
+                <div className="text-center mt-5">
+                    {!holdNFT && <Alert className='font-italic' variant={"danger"}>You don't hold any NFT from our Collection</Alert>}
                 </div>
                 <div className='mx-auto text-center mt-5'>
-                   {!connected &&  <Button onClick={connectWallet}>Connect Stoic Wallet</Button> }
-                   {connected &&  <Button onClick={disconnectWallet} variant= "danger">Disconnect Wallet</Button> }
+                    {!connected && <Button onClick={connectWallet}>Connect Stoic Wallet</Button>}
+                    {connected && <Button onClick={disconnectWallet} variant="danger">Disconnect Wallet</Button>}
                 </div>
 
             </Container>
